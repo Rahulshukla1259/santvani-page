a089dba126fa99f8bc48a2e22b73082c99993f16
Rahulshukla1259 <93595064+Rahulshukla1259@users.noreply.github.com>
2023-10-04 00:01:53 +0530

    Update Nav.js

--- src/Nav.js
+++ src/Nav.js
@@ -26,6 +26,18 @@ export default function Nav() {
             alt=""
           />
 
+              <button style={{color:"white"}}
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
